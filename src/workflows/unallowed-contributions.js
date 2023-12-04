1db3b664be9275216693f5996f08b019b80d0c39
Rachael Sewell <rachmari@github.com>
2023-12-04 22:42:03 +0000

    shebang ;

--- src/workflows/unallowed-contributions.js
+++ src/workflows/unallowed-contributions.js
@@ -1,3 +1,5 @@
-console.log('file path', process.env.FILE_PATHS)
-console.log('chnages', process.env.CHANGES)
+#!/usr/bin/env node
+
+console.log('not allowed', process.env.FILE_PATHS_NOTALLOWED)
+console.log('content types', process.env.FILE_PATHS_CONTENTTYPES)
 console.log('pr number', process.env.PR_NUMBER)
