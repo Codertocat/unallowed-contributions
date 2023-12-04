7a7ef453a55946a148113eb4bf009c1f51153ab1
Rachael Sewell <rachmari@github.com>
2023-12-04 22:58:50 +0000

    update

--- src/workflows/unallowed-contributions.js
+++ src/workflows/unallowed-contributions.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
+import { readFileSync } from 'fs'
+import yaml from 'js-yaml'
 
-console.log('not allowed', process.env.FILE_PATHS_NOTALLOWED)
-console.log('content types', process.env.FILE_PATHS_CONTENTTYPES)
-console.log('pr number', process.env.PR_NUMBER)
+const { PR_NUMBER, ORGANIZATION, REPO, FILE_PATHS_NOT_ALLOWED, FILE_PATHS_CONTENT_TYPES } = process.env
+
+
+const { notAllowed } = yaml.load(readFileSync('./src/workflows/unallowed-contribution-filters.yml', 'utf8'))
+
+main()
+async function main() {
+  const unallowedFiles = [...FILE_PATHS_NOT_ALLOWED]
+  for (const filePath of FILE_PATHS_CONTENT_TYPES) { 
+    // read fm and add to array if type is rai
+    const fileContent = readFileSync(filePath, 'utf8')
+    if (yaml.load(fileContent).data.type === 'rai') {
+      unallowedFiles.push(filePath)
+    }
+  }
+  if (unallowedFiles.length === 0) return
+
+  const reviewMessage = `👋 Hey there spelunker. It looks like you've modified some files that we can't accept as contributions:${unallowedFiles} You'll need to revert all of the files you changed that match that list using [GitHub Desktop](https://docs.github.com/en/free-pro-team@latest/desktop/contributing-and-collaborating-using-github-desktop/managing-commits/reverting-a-commit-in-github-desktop) or \`git checkout origin/main <file name>\`. Once you get those files reverted, we can continue with the review process. :octocat:\n\nThe complete list of files we can't accept are:\n${notAllowed}\n\nWe also can't accept contributions to files in the content directory with frontmatter \`type: rai\`.`
+  console.log('review message', reviewMessage)
+}
