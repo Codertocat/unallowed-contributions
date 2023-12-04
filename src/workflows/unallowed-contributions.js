a440c1a7d84c3de566826d4b6c6b6495349a56ab
Rachael Sewell <rachmari@github.com>
2023-12-04 22:29:56 +0000

    prevent dup comment

--- src/workflows/unallowed-contributions.js
+++ src/workflows/unallowed-contributions.js
@@ -1,5 +1,5 @@
 console.log('file paths not allowed', process.env.FILE_PATHS_NOTALLOWED)
-console.log('file paths not allowed', process.env.FILE_PATHS_NOTALLOWED)
+console.log('file paths content types', process.env.FILE_PATHS_CONTENTTYPES)
 console.log('file path', process.env.FILE_PATHS)
 console.log('chnages', process.env.CHANGES)
 console.log('pr number', process.env.PR_NUMBER)
