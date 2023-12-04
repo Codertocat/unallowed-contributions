8938437023fbdf20795087e0b50a1a11f11ef1b5
Rachael Sewell <rachmari@github.com>
2023-12-04 22:01:01 +0000

    update console logs

--- src/workflows/unallowed-contributions.js
+++ src/workflows/unallowed-contributions.js
@@ -1,12 +1,4 @@
-import { program } from 'commander'
+const { FILE_PATHS, PR_NUMBER } = process.env
 
-program
-  .description('Check if file paths match the exclude list and comment with a message if they do')
-  .option('--paths', 'Paths to check for changes')
-  .option('--pr', "Pull request number to post a comment on")
-  .parse(process.argv)
-
-const { paths, pr } = program.opts()
-
-console.log('paths', paths)
-console.log('pr number', pr)
+console.log('file paths', FILE_PATHS)
+console.log('pr number', PR_NUMBER)
