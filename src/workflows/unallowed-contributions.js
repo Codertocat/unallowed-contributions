d01493d9672d662e37cb627b7229ed258c042168
Rachael Sewell <rachmari@github.com>
2023-12-05 00:31:25 +0000

    test out full workflow

--- src/workflows/unallowed-contributions.js
+++ src/workflows/unallowed-contributions.js
@@ -1,27 +1,49 @@
 #!/usr/bin/env node
-const fs = require('fs')
-const matter = require('gray-matter')
+
+const coreLib = require('@actions/core')
+const { readFileSync } = require('fs')
 const yaml = require('js-yaml')
-const path = require('path')
+const { matter } = require('gray-matter')
 
+const github = require('#src/workflows/github.js')
+
+const core = coreLib
 const { PR_NUMBER, ORGANIZATION, REPO, FILE_PATHS_NOT_ALLOWED, FILE_PATHS_CONTENT_TYPES } = process.env
+const octokit = github()
 
-const { notAllowed } = yaml.load(fs.readFileSync('./src/workflows/unallowed-contribution-filters.yml', 'utf8'))
+const { notAllowed } = yaml.load(readFileSync('./src/workflows/unallowed-contribution-filters.yml', 'utf8'))
 
 main()
 async function main() {
+
   const unallowedChangedFiles = [...JSON.parse(FILE_PATHS_NOT_ALLOWED)]
   for (const filePath of JSON.parse(FILE_PATHS_CONTENT_TYPES)) { 
-    const { data } = matter(fs.readFileSync(`./${filePath}`, 'utf8'))
+    const { data } = matter(readFileSync(`./${filePath}`, 'utf8'))
     if (data.type === 'rai') {
       unallowedChangedFiles.push(filePath)
     }
   }
   if (unallowedChangedFiles.length === 0) return
 
-  const formattedUnallowedChangedFiles = `\n- ${unallowedChangedFiles.join('\n- ')}\n`
-  const formattedNotAllowed = `\n - ${notAllowed.join('\n -')}\n`
+  const listUnallowedChangedFiles = `\n- ${unallowedChangedFiles.join('\n- ')}\n`
+  const listNotAllowed = `\n - ${notAllowed.join('\n -')}\n`
 
-  const reviewMessage = `👋 Hey there spelunker. It looks like you've modified some files that we can't accept as contributions:${formattedUnallowedChangedFiles}\n You'll need to revert all of the files you changed that match that list using [GitHub Desktop](https://docs.github.com/en/free-pro-team@latest/desktop/contributing-and-collaborating-using-github-desktop/managing-commits/reverting-a-commit-in-github-desktop) or \`git checkout origin/main <file name>\`. Once you get those files reverted, we can continue with the review process. :octocat:\n\nThe complete list of files we can't accept are:${formattedNotAllowed}\nWe also can't accept contributions to files in the content directory with frontmatter \`type: rai\`.`
-  console.log('review message', reviewMessage)
+  const reviewMessage = `👋 Hey there spelunker. It looks like you've modified some files that we can't accept as contributions:${listUnallowedChangedFiles}\n You'll need to revert all of the files you changed that match that list using [GitHub Desktop](https://docs.github.com/en/free-pro-team@latest/desktop/contributing-and-collaborating-using-github-desktop/managing-commits/reverting-a-commit-in-github-desktop) or \`git checkout origin/main <file name>\`. Once you get those files reverted, we can continue with the review process. :octocat:\n\nThe complete list of files we can't accept are:${listNotAllowed}\nWe also can't accept contributions to files in the content directory with frontmatter \`type: rai\`.`
+
+  let workflowFailMessage = "It looks like you've modified some files that we can't accept as contributions."
+  let createdComment
+  try {
+      createdComment = await octokit.rest.issues.createComment({
+      owner: ORGANIZATION,
+      repo: REPO,
+      issue_number: PR_NUMBER,
+      body: reviewMessage,
+    })
+
+    workflowFailMessage = `${workflowFailMessage} Please see ${createdComment.data.html_url} for details.`
+  } catch(err) {
+    console.log("Error creating comment.", err)
+  }
+
+  core.setFailed(workflowFailMessage)
 }
